38b0b4842da4fb49a25cd33b6b854a49fd773075
Valmir Dimas <valmirdimas@gmail.com>
2021-05-31 06:25:17 -0300

    Naming Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 MailsViewer::Engine.routes.draw do
   controller "home" do
-    get "/index", to: :index
-    get "/raw", to: :raw
-    get "/html", to: :html
-    get "/plain", to: :plain
-    get '/attachment', to: :attachment
+    get "/index", action: :index, as: :mails_viewer_home_index
+    get "/raw", action: :raw, as: :mails_viewer_home_raw
+    get "/html", action: :html, as: :mails_viewer_home_html
+    get "/plain", action: :plain, as: :mails_viewer_home_plain
+    get '/attachment', action: :attachment, as: :mails_viewer_home_attachment
   end
   root to: 'home#index'
 end
